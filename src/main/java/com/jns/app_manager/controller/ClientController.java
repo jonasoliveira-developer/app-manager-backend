5cbb5bcacce67b8f338074b83fa88ecfd2d017ab
jonasoliveira-developer <jonasoliveira.webdeveloper@gmail.com>
2025-07-22 23:52:17 -0300

    feat: create client controller

--- src/main/java/com/jns/app_manager/controller/ClientController.java
+++ src/main/java/com/jns/app_manager/controller/ClientController.java
@@ -1,4 +1,51 @@
+
 package com.jns.app_manager.controller;
 
+
+import com.jns.app_manager.dtos.ClientRequestDTO;
+import com.jns.app_manager.dtos.ClientResponseDTO;
+import com.jns.app_manager.service.ClientService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/clients")
+@RequiredArgsConstructor
 public class ClientController {
+
+    private final ClientService service;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ClientResponseDTO> findById(@PathVariable UUID id) {
+        return ResponseEntity.ok(service.findById(id));
+    }
+
+    @GetMapping("list/{userId}")
+    public ResponseEntity<List<ClientResponseDTO>> findAllByUser(@PathVariable UUID userId) {
+        return ResponseEntity.ok(service.findAllByUser(userId));
+    }
+
+    @PostMapping
+    public ResponseEntity<ClientResponseDTO> save(@RequestBody @Valid ClientRequestDTO dto) {
+        var saved = service.save(dto);
+        return ResponseEntity.ok(saved);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ClientResponseDTO> update(@PathVariable UUID id,
+                                                    @RequestBody @Valid ClientRequestDTO dto) {
+        var updated = service.update(id, dto);
+        return ResponseEntity.ok(updated);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable UUID id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
